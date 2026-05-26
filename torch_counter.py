f2d92a1baad96fd050e8162ade8a9d87b0e70d1a
aaron <aaronjobac@gmail.com>
2026-05-26 16:19:14 +0200

    Add torch_counter module to use PyTorch native RNG count functions

---
+++ torch_counter.py
@@ -0,0 +1,42 @@
+import ctypes
+import os
+import sys
+import torch
+
+def count_rand_calls_enabled() -> bool:
+    return os.environ.get("COUNT_RAND_CALLS") not in (None, "0", "false", "False", "", "no", "NO", "off", "OFF")
+
+if not count_rand_calls_enabled():
+    sys.exit("Using torch_counter requires having COUNT_RAND_CALLS env var to be enabled."
+             " Counters will remain at 0 unless enabled.")
+
+_lib = None
+
+def _init_lib():
+    global _lib
+    if _lib is not None:
+        return
+
+    native_torch_lib_directory = os.path.join(os.path.dirname(torch.__file__), 'lib')
+    native_torch_lib_path = os.path.join(native_torch_lib_directory, 'libtorch_cpu.so')
+
+    if not os.path.exists(native_torch_lib_path):
+        sys.exit(f"Couldn't find native torch library at: {native_torch_lib_path}")
+
+    _lib = ctypes.CDLL(native_torch_lib_path)
+
+    _lib.get_pytorch_rng_call_count.argtypes = []
+    _lib.get_pytorch_rng_call_count.restype = ctypes.c_uint64
+
+    _lib.reset_pytorch_rng_call_count.argtypes = []
+    _lib.reset_pytorch_rng_call_count.restype = None
+
+
+def get_count() -> int:
+    _init_lib()
+    return _lib.get_pytorch_rng_call_count()
+
+
+def reset_count() -> None:
+    _init_lib()
+    _lib.reset_pytorch_rng_call_count()
